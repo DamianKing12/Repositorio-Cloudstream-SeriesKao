3510bf40c5f059c07d1250e059bd2e1a3141baf1
DamianKing12 <damian.a.king.r.12@gmail.com>
2025-12-20 12:09:12 -0400

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -3,36 +3,22 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
 
-// Use an integer for version numbers
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
-    language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    description = "Plugin para SeriesKao"
+    authors = listOf("DamianKing12")
+    status = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    requiresResources = false
+    language = "es"
+    iconUrl = "https://www.google.com/s2/favicons?domain=serieskao.top&sz=%size%"
 }
 
 android {
+    namespace = "com.DamianKing12"
     buildFeatures {
         buildConfig = true
-        viewBinding = true
+        viewBinding = false
     }
 }
