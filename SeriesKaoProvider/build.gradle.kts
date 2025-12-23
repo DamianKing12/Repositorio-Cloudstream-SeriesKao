cfbaa1d199d959b966188301da4cfbb41126d4fa
DamianKing12 <damian.a.king.r.12@gmail.com>
2025-12-22 22:08:33 -0400

    Update build.gradle.kts

--- SeriesKaoProvider/build.gradle.kts
+++ SeriesKaoProvider/build.gradle.kts
@@ -1,6 +1,9 @@
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    
+    // ✅ VERSIÓN FIJA - NO USES pre-release
+    implementation("com.lagradost:cloudstream3:4.6.0")
 }
 
 version = 1
