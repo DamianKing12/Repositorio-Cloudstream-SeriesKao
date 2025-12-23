cbe5f1ec36a27612fbaf483d6d8bd30475024990
deivijosetorresramirez24-bit <deivijosetorresramirez24@gmail.com>
2025-12-23 12:59:04 -0400

    Update build.gradle.kts

--- SeriesKaoProvider/build.gradle.kts
+++ SeriesKaoProvider/build.gradle.kts
@@ -1,9 +1,22 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("com.lagradost.cloudstream3.gradle")
 }
 
-version = 1
+android {
+    namespace = "com.DamianKing12"
+    
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 35
+    }
+    
+    buildFeatures {
+        buildConfig = true
+        viewBinding = false
+    }
+}
 
 cloudstream {
     description = "Plugin para SeriesKao"
@@ -15,10 +28,15 @@ cloudstream {
     iconUrl = "https://www.google.com/s2/favicons?domain=serieskao.top&sz=%size%"
 }
 
-android {
-    namespace = "com.DamianKing12"
-    buildFeatures {
-        buildConfig = true
-        viewBinding = false
-    }
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    
+    // USAMOS LA RUTA DIRECTA DE GITHUB PARA EVITAR ERRORES DE JITPACK
+    implementation("com.github.recloudstream:cloudstream:pre-release")
+    
+    implementation(kotlin("stdlib"))
+    implementation("com.github.Blatzar:NiceHttp:0.4.11")
+    implementation("org.jsoup:jsoup:1.18.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 }
