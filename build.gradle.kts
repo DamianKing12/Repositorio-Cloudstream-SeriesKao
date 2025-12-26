575c4093617c1834312d89b60a9d4022200f24c4
PC <damian.a.king.r.12@gmail.com>
2025-12-26 08:58:44 -0400

    5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,27 @@
+plugins {
+    id("com.lagradost.cloudstream3.gradle") version "1.0.0" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        // Esta línea es la que permite que exista el comando "make"
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
 buildscript {
     repositories {
         google()
