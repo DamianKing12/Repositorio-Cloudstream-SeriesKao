77deef2f22ab51dc2dc98d9de6e524bb547af260
DamianKing12 <damian.a.king.r.12@gmail.com>
2025-12-26 09:07:24 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,16 @@
-// 1. EL BLOQUE BUILDSCRIPT SIEMPRE PRIMERO Y SOLO UNO
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven("https://jitpack.io") // IMPORTANTE: Aquí es donde Gradle busca el plugin de Cloudstream
     }
     dependencies {
+        // Esta línea es la que realmente importa para que funcione el comando 'make'
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
 
-// 2. PLUGINS (Opcional en la raíz si se aplican en los subproyectos)
-plugins {
-    id("com.lagradost.cloudstream3.gradle") version "1.0.0" apply false
-}
-
-// 3. REPOSITORIOS PARA TODOS LOS PROYECTOS
 allprojects {
     repositories {
         google()
@@ -25,7 +19,9 @@ allprojects {
     }
 }
 
-// 4. TAREAS DE LIMPIEZA
+// Eliminamos el bloque plugins { ... } de aquí arriba porque 
+// ya lo estamos inyectando vía classpath en el buildscript.
+
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
